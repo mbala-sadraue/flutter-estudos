bd16a911e1bfad962120018db4033f709d99220a
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-08-05 09:50:02 +0100

    criar class para banco de dados local

---
+++ database/lib/database/local/database.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+
+class DB{
+
+  DB._();
+
+  static final DB instance = DB._();
+
+  static Database?_database;
+
+  get database{
+
+    if(_database != null){
+
+      return _database;
+    }
+
+    _database = _initDatabase()
+}
+
+  Future<Database> _initDatabase() async {
+
+
+    final pathData =   join(await getDatabasesPath(),'database.db');
+
+    return openDatabase(
+      pathData,
+      version: 1,
+      onCreate: _createDatabase
+    );
+  } 
+
+
+  Future _createDatabase(Database db, int version){
+
+    return await db.execute(sql);
+
+  }
+}
