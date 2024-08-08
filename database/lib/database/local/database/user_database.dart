652f821f2168eb71e4d69b5d0323ee581d62e896
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-08-08 19:16:37 +0100

    criar metodo de criação de usuario

--- database/lib/database/local/database/user_database.dart
+++ database/lib/database/local/database/user_database.dart
@@ -2,18 +2,12 @@ import 'package:database/database/local/database.dart';
 import 'package:database/database/local/sql/user_database.dart';
 import 'package:database/models/user.dart';
 
-class UserDatabase{
-
-
-
-  UserCreate(User user)async{
-
+class UserDatabase {
+  Future userCreate(User user) async {
     final db = await DB.instance.database;
 
-    final values = {'name':user.name,'phone':user.phone};
+    final values = {'name': user.name, 'phone': user.phone};
     final insert = db.insert(UserFields.tableName, values);
-    print("Usuario: $insert");
-
+    return insert;
   }
-
 }
--- database/lib/models/user.dart
+++ database/lib/models/user.dart
@@ -1,4 +1,7 @@
+import 'package:database/database/local/sql/user_database.dart';
+
 class User{
+  // final int id;
   final String name;
   final String phone;
 
@@ -6,4 +9,15 @@ class User{
     required this.name,
     required this.phone
   });
+
+  factory User.fromJson(Map<String, dynamic> json){
+      return User(name: json['name'], phone: json['phone']);
+  }
+
+  Map<String, Object> toJon(){
+    return {
+      UserFields.name:name,
+      UserFields.phone:phone
+    };
+  }
 }
