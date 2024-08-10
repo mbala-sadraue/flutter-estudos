8f84fec216175308fa47c353b69da6bdaa039ff0
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-08-10 11:21:40 +0100

    criar imagem picker

--- database/lib/ui/componentes/form.dart
+++ database/lib/ui/componentes/form.dart
@@ -3,58 +3,63 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Formulario extends StatelessWidget {
- Formulario({super.key});
+  Formulario({super.key});
 
-
-  final  TextEditingController nameController =TextEditingController();
+  final TextEditingController nameController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
   final userController = Get.put(UserController());
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    final _formKey  = GlobalKey<FormState>();
     return SingleChildScrollView(
       child: Form(
-      key: _formKey,
-      child: 
-      Column(
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              label: Text("Nome")
-            ),
-            validator: (value){
-
-              if(value == null || value.isEmpty ){
-                return "O campo nome é obrigatorio";
-              }
-              return null;
-            },
-            controller:nameController , 
-               ),
-           TextFormField(
-            decoration: const InputDecoration(
-              label: Text("Telefone")
-            ),
-            validator: (value){
-              if(value == null || value.isEmpty){
-                return "O compo telefone é obrigatório";
-              }
-            },
-            controller:phoneController ,
-          ),
-          ElevatedButton(onPressed: () {
-            if(_formKey.currentState!.validate()){
-
-           userController.userCreate(nameController.text, phoneController.text);
-           nameController.clear();
-           phoneController.clear();
-            }
-          }, child: const  Text("cadastrar"))
-        ],
-      )),
+          key: _formKey,
+          child: Column(
+            children: [
+               Column(
+                children: [
+                 const   CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    maxRadius: 40,
+                  ),
+                  TextButton(onPressed: (){}, child: const  Text("Adicionar imagem"))
+                ],
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text("Nome")),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "O campo nome é obrigatorio";
+                  }
+                  return null;
+                },
+                controller: nameController,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(label: Text("Telefone")),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "O compo telefone é obrigatório";
+                  }
+                },
+                controller: phoneController,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    userController.userCreate(
+                        nameController.text, phoneController.text);
+                    nameController.clear();
+                    phoneController.clear();
+                  }
+                },
+                child: const Text("cadastrar"),
+              )
+            ],
+          )),
     );
   }
-
-  
-
 }
