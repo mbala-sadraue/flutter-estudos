43a9dc18c0af0af181b6219bc56891cbaa4b656b
Mbala Sadraque <orlandosadraquiorlandinho9@gmail.com>
2024-08-26 11:01:31 +0100

    terminar o meu bloc decontador

--- state/lib/bloc/counter_bloc.dart
+++ state/lib/bloc/counter_bloc.dart
@@ -6,12 +6,12 @@ CounterBloc():super(0){
   on<CounterIncrementPressed>(_onIcrementValue);
 on<CounterDecrementPressed>(_onDecrementValue);
 }
-_onIcrementValue( CounterIncrementPressed event, Emitter emit){
-    emit(state+1);
-}
+_onIcrementValue( CounterIncrementPressed event, Emitter emit) => emit(state+1);
 
-_onDecrementValue( CounterDecrementPressed event, Emitter emit){
-    emit(state-1);
+
+void _onDecrementValue( CounterDecrementPressed event, Emitter emit){
+   if(state > 0) {
+    emit(state-1);}
 }
 
 }
--- state/lib/main.dart
+++ state/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state/bloc/counter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application. 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,24 +18,54 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key,required this.title});
- 
+   MyHomePage({super.key, required this.title});
+  
+  final counterBloc = CounterBloc();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-    body: Center(
-      child:  Text("Ola"),
-    ),
+    return BlocProvider<CounterBloc>(
+      create: (context) => CounterBloc(),
+      child: BlocBuilder<CounterBloc, int>(
+        builder: (context, state) {
+
+          final counterBloc = BlocProvider.of<CounterBloc>(context); 
+
+          return Scaffold(
+            body: Center(
+              child: Text(state.toString()),
+            ),
+            floatingActionButton: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    counterBloc.add(CounterIncrementPressed());
+                  },
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                   counterBloc.add(CounterDecrementPressed());
+                  },
+                  child: const Icon(Icons.remove),
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
-
 }
-
